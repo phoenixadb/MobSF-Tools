13bc228fa85d2945b5da92bb5b300e454a753335
DE BOCK Arnaud <arnaud.debock@orange.com>
2019-09-09 10:58:37 +0200

    Init

---
+++ ansible/roles/mobsf/files/mobsf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Launch MobSF
+
+export LANG=en_US.UTF-8
+source ~/.bash_profile
+
+echo 'MobSF is launching'
+
+cd $HOME/Mobile-Security-Framework-MobSF
+. venv/bin/activate 
+gunicorn -b 127.0.0.1:8000 MobSF.wsgi:application --workers=1 --threads=4 --timeout=1800 >> ~/Library/Logs/mobsf/mobsf.log &
+
+echo 'MobSF launched'
