e6925277db64b304ca5d6ae599e26ce471dbc944
superpoussin22 <vincent.nadal@orange.fr>
2019-09-24 05:16:56 +0200

    Update mobsf.sh
    more threads

--- ansible/roles/mobsf/files/mobsf.sh
+++ ansible/roles/mobsf/files/mobsf.sh
@@ -8,6 +8,6 @@ echo 'MobSF is launching'
 
 cd $HOME/Mobile-Security-Framework-MobSF
 . venv/bin/activate 
-gunicorn -b 127.0.0.1:8000 MobSF.wsgi:application --workers=1 --threads=4 --timeout=1800 >> ~/Library/Logs/mobsf/mobsf.log &
+gunicorn -b 127.0.0.1:8000 MobSF.wsgi:application --workers=1 --threads=10 --timeout=1800 >> ~/Library/Logs/mobsf/mobsf.log &
 
 echo 'MobSF launched'
